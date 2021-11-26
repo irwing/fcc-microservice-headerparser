e857648be650f4d4d2a54e60579730a6a2f35472
Irwing Naranjo <khalisser@gmail.com>
2021-11-26 20:56:54 -0300

    Header Parser microservice

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,15 +19,25 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/whoami", function (req, res) {
 
-// your first API endpoint... 
+  var userAgent = req.headers['user-agent'];
+  var acceptLanguage = req.headers["accept-language"];
+
+  res.json({
+    "ipaddress": req.ip,
+    "language": acceptLanguage,
+    "software": userAgent
+  });
+});
+
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+// process.env.PORT
+var listener = app.listen(43000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
